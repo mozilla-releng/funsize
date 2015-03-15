fdbdb48488e610a9af2b6b424a913150716b3453
Rail Aliiev <rail@mozilla.com>
2015-03-15 11:25:38 -0400

    Log to a configurable directory

--- scheduler/lib/logging.js
+++ scheduler/lib/logging.js
@@ -1,5 +1,6 @@
 import winston from 'winston';
 import path from 'path';
+import config from '../config/rail';
 
 export var log = new (winston.Logger)(
   {transports: [
@@ -9,7 +10,7 @@ export var log = new (winston.Logger)(
       prettyPrint: true
     }),
     new (winston.transports.DailyRotateFile)({
-      filename: path.join(__dirname, "../logs/funsize.log"),
+      filename: path.join(config.funsize.logDir, "funsize.log"),
       level: 'debug',
       json: false,
       prettyPrint: true
