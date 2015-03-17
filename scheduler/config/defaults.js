5d119feae0d1902d2aa7a9f0c950a0b4dced761a
Rail Aliiev <rail@mozilla.com>
2015-03-17 14:14:46 -0400

    Add Heroku files

--- scheduler/config/defaults.js
+++ scheduler/config/defaults.js
@@ -1,21 +1,22 @@
 module.exports = {
   balrog: {
+    api_root: process.env.BALROG_API_ROOT,
     credentials: {
-      username: null,
-      password: null
+      username: process.env.BALROG_PASSWORD,
+      password: process.env.BALROG_PASSWORD
     }
   },
   pulse: {
     credentials: {
-      username: null,
-      password: null
+      username: process.env.PULSE_USERNAME,
+      password: process.env.PULSE_PASSWORD
     },
-    queueName: 'funsize'
+    queueName: process.env.FUNSIZE_QUEUE_NAME
   },
   taskcluster: {
     credentials: {
-      clientId: null,
-      accessToken: null
+      clientId: process.env.TASKCLUSTER_CLIENT_ID,
+      accessToken: process.env.TASKCLUSTER_ACCESS_TOKEN
     }
   },
   funsize: {
