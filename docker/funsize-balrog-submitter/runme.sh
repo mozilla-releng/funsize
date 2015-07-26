98ff10b5510522c8f9ee885b0e0495ad9f250067
Rail Aliiev <rail@mozilla.com>
2015-07-26 08:47:51 -0400

    Bug 1187612 - Retry if queue.taskcluster.net returns 5xx errors

--- docker/funsize-balrog-submitter/runme.sh
+++ docker/funsize-balrog-submitter/runme.sh
@@ -6,7 +6,8 @@ test $PARENT_TASK_ARTIFACTS_URL_PREFIX
 test $BALROG_API_ROOT
 test $SIGNING_CERT
 
-wget -O /home/worker/manifest.json "$PARENT_TASK_ARTIFACTS_URL_PREFIX/manifest.json"
+curl --retry 10 --retry-delay 10 -o /home/worker/manifest.json \
+    "$PARENT_TASK_ARTIFACTS_URL_PREFIX/manifest.json"
 cat /home/worker/manifest.json
 python /home/worker/bin/funsize-balrog-submitter.py \
     --artifacts-url-prefix "$PARENT_TASK_ARTIFACTS_URL_PREFIX" \
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="funsize",
-    version="0.21",
+    version="0.22",
     description="Funsize Scheduler",
     author="Mozilla Release Engineering",
     packages=["funsize"],
