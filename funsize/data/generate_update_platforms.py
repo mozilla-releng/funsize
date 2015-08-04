2fc66c4e1a18afb2ce1df8473770c9cf2fe9741c
Rail Aliiev <rail@mozilla.com>
2015-08-04 04:24:51 -0400

    Refresh platform_map.json

--- funsize/data/generate_update_platforms.py
+++ funsize/data/generate_update_platforms.py
@@ -1,5 +1,5 @@
 import json
 from platforms import update_platform_map
 
-with open("platforms.js", "w") as f:
+with open("platform_map.json", "w") as f:
     json.dump(update_platform_map, f, indent=2, sort_keys=True)
