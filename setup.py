2d5c98a481c0be29dc6187cf04bd89af8cfc2bde
Rail Aliiev <rail@mozilla.com>
2015-08-05 20:23:15 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="funsize",
-    version="0.34",
+    version="0.35",
     description="Funsize Scheduler",
     author="Mozilla Release Engineering",
     packages=["funsize"],
