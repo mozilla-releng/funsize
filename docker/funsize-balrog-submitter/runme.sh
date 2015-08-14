d32fadf7865912e453c57d6325bc82fe82a670a9
Rail Aliiev <rail@mozilla.com>
2015-08-14 12:36:58 -0400

    Bug 1194762 - Publish manifest in balrog task

--- docker/funsize-balrog-submitter/runme.sh
+++ docker/funsize-balrog-submitter/runme.sh
@@ -6,12 +6,16 @@ test $PARENT_TASK_ARTIFACTS_URL_PREFIX
 test $BALROG_API_ROOT
 test $SIGNING_CERT
 
-curl --location --retry 10 --retry-delay 10 -o /home/worker/manifest.json \
+ARTIFACTS_DIR="/home/worker/artifacts"
+mkdir -p "$ARTIFACTS_DIR"
+
+curl --location --retry 10 --retry-delay 10 -o "$ARTIFACTS_DIR/manifest.json" \
     "$PARENT_TASK_ARTIFACTS_URL_PREFIX/manifest.json"
-cat /home/worker/manifest.json
+
+cat "$ARTIFACTS_DIR/manifest.json"
 python /home/worker/bin/funsize-balrog-submitter.py \
     --artifacts-url-prefix "$PARENT_TASK_ARTIFACTS_URL_PREFIX" \
-    --manifest /home/worker/manifest.json \
+    --manifest "$ARTIFACTS_DIR/manifest.json" \
     -a "$BALROG_API_ROOT" \
     --signing-cert "/home/worker/keys/${SIGNING_CERT}.pubkey" \
     --verbose \
