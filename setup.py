96d4d90350a8a1ee378f1b0dddfcd45acd4bff53
Rail Aliiev <rail@mozilla.com>
2017-08-30 16:55:46 +0300

    No expires (#82)
    * Do not set task `expires`
    
    It causes some issues with artifact uploads. By default Tascluster sets
    this field to `deadline` + 1 year.
    
    * Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="funsize",
-    version="0.94",
+    version="0.95",
     description="Funsize Scheduler",
     author="Mozilla Release Engineering",
     packages=["funsize"],
